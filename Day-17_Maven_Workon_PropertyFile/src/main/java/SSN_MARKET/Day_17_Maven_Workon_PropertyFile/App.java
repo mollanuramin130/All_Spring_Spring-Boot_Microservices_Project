342525dc7fe320df9155170b38ef6a71f2fa9072
Nur Amin Molla <mollanuramin130@NURs-MacBook-Pro.local>
2025-07-22 17:26:36 +0530

    4th U[date success

---
+++ Day-17_Maven_Workon_PropertyFile/src/main/java/SSN_MARKET/Day_17_Maven_Workon_PropertyFile/App.java
@@ -0,0 +1,10 @@
+package SSN_MARKET.Day_17_Maven_Workon_PropertyFile;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
