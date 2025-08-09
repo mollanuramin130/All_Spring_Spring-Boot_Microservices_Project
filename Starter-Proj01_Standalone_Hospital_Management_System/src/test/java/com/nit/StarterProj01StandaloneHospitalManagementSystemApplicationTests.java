97958c3c299c008f3443588502cf1dc5bb2b4f8f
mollanuramin130 <mollanuramin130@192.168.0.116>
2025-08-09 23:49:15 +0530

    2nd commit

---
+++ Starter-Proj01_Standalone_Hospital_Management_System/src/test/java/com/nit/StarterProj01StandaloneHospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterProj01StandaloneHospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
